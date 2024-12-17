939f2376b4cc5e8210371c0fae87ad0a84b0285d
jnewgard <44853169+jnewgard@users.noreply.github.com>
2024-12-17 13:19:31 -0800

    Create project (#1)
    * Add initial project
    
    * Add code configs
    
    * Update readme & sdks
    
    * Improve UX
    
    * Add github configs
    
    * Update sdks & rename project
    
    * Update cards sdk

---
+++ app/src/main/java/com/mcards/sdk/fm/demo/DemoFragment.kt
@@ -0,0 +1,225 @@
+package com.mcards.sdk.fm.demo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.mcards.sdk.auth.AuthSdk
+import com.mcards.sdk.auth.AuthSdkProvider
+import com.mcards.sdk.auth.model.auth.User
+import com.mcards.sdk.cards.CardsSdk
+import com.mcards.sdk.cards.CardsSdkProvider
+import com.mcards.sdk.core.model.AuthTokens
+import com.mcards.sdk.core.model.card.Card
+import com.mcards.sdk.core.network.SdkResult
+import com.mcards.sdk.fm.FmSdk
+import com.mcards.sdk.fm.FmSdkFactory
+import com.mcards.sdk.fm.demo.databinding.FragmentDemoBinding
+import com.mcards.sdk.fm.model.FmArgs
+import com.mcards.sdk.fm.model.features.Feature
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.disposables.Disposable
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class DemoFragment : Fragment() {
+
+    private var _binding: FragmentDemoBinding? = null
+    private val binding get() = _binding!!
+    private val fmSdk = FmSdkFactory.get()
+    private val cardsSdk = CardsSdkProvider.getInstance()
+
+    private var userPhoneNumber = ""
+    private var accessToken = ""
+    private var idToken = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDemoBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val authSdk = AuthSdkProvider.getInstance()
+
+        val loginCallback = object : AuthSdk.LoginCallback {
+            override fun onSuccess(
+                user: User,
+                tokens: AuthTokens,
+                regionChanged: Boolean,
+                cardId: String?
+            ) {
+                accessToken = tokens.accessToken
+                idToken = tokens.idToken
+                userPhoneNumber = user.userClaim.phoneNumber
+                initSdks(tokens)
+            }
+
+            override fun onFailure(message: String) {
+                activity?.runOnUiThread {
+                    Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        binding.loginBtn.setOnClickListener {
+            if (userPhoneNumber.isBlank()) {
+                authSdk.login(requireContext(), loginCallback)
+            } else {
+                authSdk.login(requireContext(), userPhoneNumber, loginCallback)
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun initSdks(tokens: AuthTokens) {
+        cardsSdk.init(requireActivity(),
+            tokens.accessToken,
+            debug = true,
+            useFirebase =  false,
+            object : CardsSdk.InvalidTokenCallback {
+                override fun onTokenInvalid(): String {
+                    return AuthSdkProvider.getInstance().refreshTokens().accessToken
+                }
+            })
+
+        val tokenCallback = object : FmSdk.InvalidTokenCallback {
+            override fun onTokenInvalid(): AuthTokens {
+                return AuthSdkProvider.getInstance().refreshTokens()
+            }
+        }
+
+        val syncCallback = object : FmSdk.SyncCallback {
+            override fun onFailure(msg: String) {
+                activity?.runOnUiThread {
+                    Snackbar.make(requireView(), msg, BaseTransientBottomBar.LENGTH_LONG).show()
+                    binding.progressbar.visibility = View.GONE
+                }
+            }
+
+            override fun onSubscribe() {
+                activity?.runOnUiThread {
+                    binding.progressbar.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onSuccess() {
+                activity?.runOnUiThread {
+                    binding.progressbar.visibility = View.GONE
+                }
+                //TODO take any needed action now that the FMSDK is successfully synced
+            }
+        }
+
+        val args = FmArgs("programId", requireActivity(), tokenCallback, syncCallback)
+        fmSdk.init(args)
+        fmSdk.setTokens(tokens)
+
+        getCards()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun getCards() {
+        cardsSdk.getCardsList()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : SingleObserver<SdkResult<List<Card>>> {
+                override fun onSubscribe(d: Disposable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.VISIBLE
+                    }
+                }
+
+                override fun onError(e: Throwable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                        Snackbar.make(
+                            requireView(),
+                            e.localizedMessage!!,
+                            BaseTransientBottomBar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onSuccess(t: SdkResult<List<Card>>) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                    }
+                    t.result?.let {
+                        if (it.isNotEmpty()) {
+                            val card = it[0]
+                            getFeatures(card)
+                        }
+                    } ?: t.errorMsg?.let {
+                        activity?.runOnUiThread {
+                            Snackbar.make(requireView(), it, BaseTransientBottomBar.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+            })
+    }
+
+    @SuppressLint("CheckResult")
+    private fun getFeatures(card: Card) {
+        fmSdk.setCard(card)
+        fmSdk.features.getFeatures(card.uuid!!, Feature.Status.ALL)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : SingleObserver<SdkResult<Array<Feature>>> {
+                override fun onSubscribe(d: Disposable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.VISIBLE
+                    }
+                }
+
+                override fun onError(e: Throwable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                        Snackbar.make(
+                            requireView(),
+                            e.localizedMessage!!,
+                            BaseTransientBottomBar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onSuccess(t: SdkResult<Array<Feature>>) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                    }
+                    t.result?.let {
+                        if (it.isNotEmpty()) {
+                            val feature = it[0]
+                            activity?.runOnUiThread {
+                                MaterialAlertDialogBuilder(requireContext())
+                                    .setTitle("Success")
+                                    .setMessage(getString(R.string.success, it.size))
+                                    .setPositiveButton("Ok") { dialog, _ ->
+                                        dialog.dismiss()
+                                    }.create().show()
+                            }
+                        }
+                    } ?: t.errorMsg?.let {
+                        activity?.runOnUiThread {
+                            Snackbar.make(requireView(), it, BaseTransientBottomBar.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+            })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/mcards/sdk/fm/demo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.mcards.sdk.fm.demo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.mcards.sdk.auth.AuthSdkProvider
+
+//TODO replace with your auth0 aud gotten from the mCards team
+private const val AUTH0_AUD = "https://staging.mcards.com/api"
+
+//TODO replace with your auth0 client ID gotten from the mCards team
+private const val AUTH0_CLIENT_ID = "DL8XpUmzegVl9dR8QpO9djDifTY7nGyd"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // if login appears to succeed, but the 2step screen spins indefinitely without
+        // redirecting to your app, it means the auth0 scheme is incorrect.
+        AuthSdkProvider.getInstance().init(getString(R.string.auth0_domain),
+            AUTH0_CLIENT_ID,
+            AUTH0_AUD,
+            BuildConfig.APPLICATION_ID)
+    }
+}
