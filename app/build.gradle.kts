c8bd02ee8b212a6d2e043bb52c5c970f886a3194
Jake N <jnewgard@mcards.com>
2025-03-21 18:06:02 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,6 @@ android {
 }
 
 dependencies {
-    //implementation(libs.androidx.core.ktx)
-    //implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.navigation.fragment.ktx)
 
